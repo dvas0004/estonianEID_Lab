797b3943f92bb168ef54db1dac3bc40245f70692
David Vassallo <davevassallo@gmail.com>
2016-04-01 11:29:35 +0200

    Update signer.sh

--- native_messaging/host/signer.sh
+++ native_messaging/host/signer.sh
@@ -1,1 +1,1 @@
-openssl dgst -binary -sha512 /tmp/toSign | /usr/bin/pkcs15-crypt --sign --key 01 --sha-512 --pkcs1 -p 0071 --raw | base64
+openssl dgst -binary -sha512 /tmp/toSign | /usr/bin/pkcs15-crypt --sign --key 01 --sha-512 --pkcs1 -p 0000 --raw | base64
